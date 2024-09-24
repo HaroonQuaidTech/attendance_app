a3853afdb7b3cfe34c125f129bf15b3bf8e9e7d6
Your Name <your.email@example.com>
2024-09-24 12:43:01 +0500

    graphical builder view

--- lib/components/graphicalbuiler.dart
+++ lib/components/graphicalbuiler.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'package:carousel_slider/carousel_slider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:fl_chart/fl_chart.dart' hide PieChart;
@@ -13,20 +13,7 @@ class Graphicalbuiler extends StatefulWidget {
 }
 
 class _GraphicalbuilerState extends State<Graphicalbuiler> {
-    int _currentIndex = 0;
-      Widget buildDot({required int index}) {
-    return Container(
-      width: 12.0,
-      height: 4.0,
-      margin: EdgeInsets.symmetric(horizontal: 4),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12),
-        // shape: BoxShape.rectangle,
-        color: _currentIndex == index ? Color(0xff9478F7) : Colors.grey[300],
-      ),
-    );
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -36,26 +23,7 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
       child: Column(children: [
    SizedBox(height: 20,),
 
-      CarouselSlider.builder(
-          options: CarouselOptions(
-              height: 482,
-              viewportFraction: 1.0,
-              initialPage: 0,
-              enableInfiniteScroll: false,
-              reverse: false,
-              autoPlay: false,
-              autoPlayInterval: Duration(seconds: 3),
-              autoPlayAnimationDuration: Duration(milliseconds: 800),
-              enlargeCenterPage: true,
-              scrollDirection: Axis.horizontal,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              }),
-          itemCount: 4,
-          itemBuilder: (BuildContext context, int index, int realindex) {
-          return Container(
+     Container(
             height: 482,
             width: double.infinity,
             decoration: BoxDecoration(
@@ -101,7 +69,7 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
                     child: BarChart(
                       BarChartData(
                         alignment: BarChartAlignment.spaceAround,
-                        maxY: 8,
+                        maxY: 45,
                         barTouchData: BarTouchData(enabled: false),
                         titlesData: FlTitlesData(
                           leftTitles: AxisTitles(
@@ -115,7 +83,7 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
                                             fontWeight: FontWeight.w600));
                               },
                               reservedSize: 28,
-                              interval: 1,
+                              interval:5,
                             ),
                           ),
                           bottomTitles: AxisTitles(
@@ -124,25 +92,25 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
                               getTitlesWidget: (value, meta) {
                                 switch (value.toInt()) {
                                   case 0:
-                                    return Text('Mon',
+                                    return Text('W1',
                                         style: TextStyle(
                                             color: Colors.black,
                                             fontSize: 15,
                                             fontWeight: FontWeight.w600));
                                   case 1:
-                                    return Text('Tue',
+                                    return Text('W2',
                                         style: TextStyle(
                                             color: Colors.black,
                                             fontSize: 15,
                                             fontWeight: FontWeight.w600));
                                   case 2:
-                                    return Text('Wed',
+                                    return Text('W3',
                                         style: TextStyle(
                                             color: Colors.black,
                                             fontSize: 15,
                                             fontWeight: FontWeight.w600));
                                   case 3:
-                                    return Text('Thur',
+                                    return Text('W4',
                                         style: TextStyle(
                                             color: Colors.black,
                                             fontSize: 15,
@@ -153,12 +121,7 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
                                             color: Colors.black,
                                             fontSize: 15,
                                             fontWeight: FontWeight.w600));
-                                  case 5:
-                                    return Text('Sat',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 15,
-                                            fontWeight: FontWeight.w600));
+                         
                                   default:
                                     return Text('');
                                 }
@@ -179,61 +142,42 @@ class _GraphicalbuilerState extends State<Graphicalbuiler> {
                             x: 0,
                             barRods: [
                               BarChartRodData(
-                                  toY: 8, color: Color(0xff9478F7), width: 22),
+                                  toY: 40, color: Color(0xff9478F7), width: 22),
                             ],
                           ),
                           BarChartGroupData(
                             x: 1,
                             barRods: [
                               BarChartRodData(
-                                  toY: 7, color: Color(0xff9478F7), width: 22),
+                                  toY: 35, color: Color(0xff9478F7), width: 22),
                             ],
                           ),
                           BarChartGroupData(
                             x: 2,
                             barRods: [
                               BarChartRodData(
-                                  toY: 6, color: Color(0xff9478F7), width: 22),
+                                  toY: 30, color: Color(0xff9478F7), width: 22),
                             ],
                           ),
                           BarChartGroupData(
                             x: 3,
                             barRods: [
                               BarChartRodData(
-                                  toY: 5, color: Color(0xff9478F7), width: 22),
+                                  toY: 38, color: Color(0xff9478F7), width: 22),
                             ],
                           ),
-                          BarChartGroupData(
-                            x: 4,
-                            barRods: [
-                              BarChartRodData(
-                                  toY: 4, color: Color(0xff9478F7), width: 22),
-                            ],
-                          ),
-                          BarChartGroupData(
-                            x: 5,
-                            barRods: [
-                              BarChartRodData(
-                                  toY: 3, color: Color(0xff9478F7), width: 22),
-                            ],
-                          ),
+                        
+                       
                         ],
                       ),
                     ),
                   ),
-                       SizedBox(height: 14),
-              
-                            //----------------------dot indicators--------------------------------
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: List.generate(
-                                  4, (index) => buildDot(index: index)),
-                            ),
+                    
                 ],
               ),
             ),
-          );
-  }),
+          ),
+
         SizedBox(
           height: 30,
         ),
