3e1448da839614a65f0d862dbfcc706be747f884
Uzair <you@example.com>
2024-11-22 14:26:45 +0500

    splash screen cheanged

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -10,7 +10,7 @@ class Splashscreen extends StatefulWidget {
 
 class _SplashscreenState extends State<Splashscreen> {
   void _checkUserLogin() async {
-    await Future.delayed(const Duration(seconds: 5));
+    await Future.delayed(const Duration(seconds: 1));
 
     User? user = FirebaseAuth.instance.currentUser;
 
