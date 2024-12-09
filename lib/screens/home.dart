46a7802bd849dbb4c4caa376eab5b07efeca5fbb
HaroonQuaidTech <your.email@example.com>
2024-12-09 12:02:13 +0500

    UI CHANGES----

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -862,40 +862,46 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                             ),
-                            FloatingActionButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const CheckinScreen(),
-                                  ),
-                                );
-                              },
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.inversePrimary,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(300),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/checkout_button.png',
-                                    height: screenWidth * 0.06,
-                                    width: screenWidth * 0.06,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                  ),
-                                  SizedBox(height: screenHeight * 0.006),
-                                  Text(
-                                    'Check Out',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: responsiveFontSize8,
-                                      height: 0,
+                            SizedBox(
+                              width: screenSize.width * 0.17,
+                              height: screenSize.height * 0.085,
+                              child: FloatingActionButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const CheckinScreen(),
                                     ),
-                                  ),
-                                ],
+                                  );
+                                },
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .inversePrimary,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(300),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/checkout_button.png',
+                                      height: screenWidth * 0.06,
+                                      width: screenWidth * 0.06,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    SizedBox(height: screenHeight * 0.006),
+                                    Text(
+                                      'Check Out',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: responsiveFontSize8,
+                                        height: 0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
@@ -939,40 +945,46 @@ class _HomeScreenState extends State<HomeScreen> {
                                 ),
                               ),
                             ),
-                            FloatingActionButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => const CheckinScreen(),
-                                  ),
-                                );
-                              },
-                              backgroundColor:
-                                  Theme.of(context).colorScheme.inversePrimary,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(300),
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    'assets/checkin_button.png',
-                                    height: screenWidth * 0.06,
-                                    width: screenWidth * 0.06,
-                                    color:
-                                        Theme.of(context).colorScheme.primary,
-                                  ),
-                                  SizedBox(height: screenHeight * 0.006),
-                                  Text(
-                                    'Check In',
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: responsiveFontSize8,
-                                      height: 0,
+                            SizedBox(
+                              width: screenSize.width * 0.17,
+                              height: screenSize.height * 0.085,
+                              child: FloatingActionButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) =>
+                                          const CheckinScreen(),
                                     ),
-                                  ),
-                                ],
+                                  );
+                                },
+                                backgroundColor: Theme.of(context)
+                                    .colorScheme
+                                    .inversePrimary,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(300),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Image.asset(
+                                      'assets/checkin_button.png',
+                                      height: screenWidth * 0.06,
+                                      width: screenWidth * 0.06,
+                                      color:
+                                          Theme.of(context).colorScheme.primary,
+                                    ),
+                                    SizedBox(height: screenHeight * 0.006),
+                                    Text(
+                                      'Check In',
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: responsiveFontSize8,
+                                        height: 0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
