f5acc1909c4b11282116a5509ae0636a0e06f8ab
Uzair <you@example.com>
2024-12-23 12:32:56 +0500

    code merge

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:quaidtech/screens/login.dart';
@@ -915,6 +916,9 @@ class _ProfileScreenState extends State<ProfileScreen> {
                                       controller: _phoneController,
                                       keyboardType: TextInputType.phone,
                                       enabled: isEdited,
+                                      inputFormatters: [
+                                        LengthLimitingTextInputFormatter(11)
+                                      ],
                                       decoration: InputDecoration(
                                         filled: true,
                                         fillColor: Colors.white,
